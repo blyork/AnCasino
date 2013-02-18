1b48103a2c299390f2b76bd2b9f9e0078b40c5c2
Benjamin York <benjaminyork@gmail.com>
2013-02-18 02:01:01 -0600

    Reformatted code using Intellj default formatting.

--- src/main/java/me/darazo/ancasino/command/CasinoReload.java
+++ src/main/java/me/darazo/ancasino/command/CasinoReload.java
@@ -1,29 +1,28 @@
 package me.darazo.ancasino.command;
 
+import me.darazo.ancasino.AnCasino;
 import org.bukkit.entity.Player;
 
-import me.darazo.ancasino.AnCasino;
-
 public class CasinoReload extends AnCommand {
-	
-	// casino reload command
-	public CasinoReload(AnCasino plugin, String[] args, Player player) {
-		super(plugin, args, player);
-	}
-	
-	public Boolean process() {
-		
-		// Permissions
-		if(!plugin.permission.isAdmin(player)) {
-			noPermission();
-			return true;
-		}
-		
-		plugin.reloadConfig();
-		plugin.slotData.loadSlots();
-		plugin.typeData.loadTypes();
-		sendMessage("Configuration reloaded");
-		return true;
-	}
+
+    // casino reload command
+    public CasinoReload(AnCasino plugin, String[] args, Player player) {
+        super(plugin, args, player);
+    }
+
+    public Boolean process() {
+
+        // Permissions
+        if (!plugin.permission.isAdmin(player)) {
+            noPermission();
+            return true;
+        }
+
+        plugin.reloadConfig();
+        plugin.slotData.loadSlots();
+        plugin.typeData.loadTypes();
+        sendMessage("Configuration reloaded");
+        return true;
+    }
 
 }
--- src/main/java/me/darazo/ancasino/command/CasinoStats.java
+++ src/main/java/me/darazo/ancasino/command/CasinoStats.java
@@ -1,37 +1,36 @@
 package me.darazo.ancasino.command;
 
-import org.bukkit.entity.Player;
-
 import me.darazo.ancasino.AnCasino;
 import me.darazo.ancasino.util.Stat;
+import org.bukkit.entity.Player;
 
 public class CasinoStats extends AnCommand {
-	
-	// Command for listing slot machine statistics
-	public CasinoStats(AnCasino plugin, String[] args, Player player) {
-		super(plugin, args, player);
-	}
-	
-	public Boolean process() {
-		
-		// Permissions
-		if(!plugin.permission.isAdmin(player)) {
-			noPermission();
-			return true;
-		}
-		
-		sendMessage("Statistics for registered types:");
-		for(Stat stat : plugin.statsData.getStats()) {
-			
-			String type = stat.getType();
-			Integer spins = stat.getSpins();
-			Double won = stat.getWon();
-			Double lost = stat.getLost();
-			
-			sendMessage(type + " - spins: " + spins +" - money won: " + won + " - money lost: " + lost);
-		}
-		
-		return true;
-	}
+
+    // Command for listing slot machine statistics
+    public CasinoStats(AnCasino plugin, String[] args, Player player) {
+        super(plugin, args, player);
+    }
+
+    public Boolean process() {
+
+        // Permissions
+        if (!plugin.permission.isAdmin(player)) {
+            noPermission();
+            return true;
+        }
+
+        sendMessage("Statistics for registered types:");
+        for (Stat stat : plugin.statsData.getStats()) {
+
+            String type = stat.getType();
+            Integer spins = stat.getSpins();
+            Double won = stat.getWon();
+            Double lost = stat.getLost();
+
+            sendMessage(type + " - spins: " + spins + " - money won: " + won + " - money lost: " + lost);
+        }
+
+        return true;
+    }
 
 }
